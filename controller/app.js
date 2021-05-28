d980cfdcda2c57eab9baf156ef74db3cea2dcffd
t-comala <t-comala@microsoft.com>
2021-05-28 21:34:34 +0300

    change app.js

--- controller/app.js
+++ controller/app.js
@@ -1,37 +1,52 @@
-//imports modules
-const express = require('express')
-const json2html = require('node-json2html');
-const fileUpload = require('express-fileupload')
-const model = require('../model/detectAnomalies')
-const lineReader = require('n-readlines');
-const FormData = require('form-data')
-const fetch= require('node-fetch')
-const path = require('path');
+function importModules() {
+    const express = require('express')
+    const json2html = require('node-json2html');
+    const fileUpload = require('express-fileupload')
+    const model = require('../model/detectAnomalies')
+    const lineReader = require('n-readlines');
+    const FormData = require('form-data')
+    const fetch = require('node-fetch')
+    const path = require('path');
+    return {express, json2html, fileUpload, model, lineReader, FormData, fetch, path};
+}
+
+const {express, json2html, fileUpload, model, lineReader, FormData, fetch, path} = importModules();
 const app = express()
 const serverPort = 8080;
 
-//define app uses
-app.use(express.urlencoded({
-    extended: false
-}))
-app.use(fileUpload({}))
-app.use(express.static('view'))
+function StartAppUsing() {
+    app.use(express.urlencoded({
+        extended: false
+    }))
+    app.use(fileUpload({}))
+    app.use(express.static('view'))
+}
 
-//Get Method for '/' url
-app.get('/', (req, res) => {
-    res.sendFile('view/index.html')
-})
-app.post('/', (req, res) => {
-    //get values from view
-    let trainFile = req.files.trainSetInput
-    let testSetInput = req.files.testSetInput
-    let algorithmType = req.body.chosenAlgorithm
-    model.detectAnomalies(trainFile.data.toString(), testSetInput.data.toString(), algorithmType).then((result) => {
-        res.contentType("application/json")
-        res.send(JSON.stringify(result.anomalies))
-        res.end()
+StartAppUsing();
+
+function AppGet() {
+    app.get('/', (req, res) => {
+        res.sendFile('view/index.html')
     })
-})
+}
+
+AppGet();
+
+function appPostAnomalies() {
+    app.post('/', (req, res) => {
+        //get values from view
+        let trainFile = req.files.trainSetInput
+        let testSetInput = req.files.testSetInput
+        let algorithmType = req.body.chosenAlgorithm
+        model.detectAnomalies(trainFile.data.toString(), testSetInput.data.toString(), algorithmType).then((result) => {
+            res.contentType("application/json")
+            res.send(JSON.stringify(result.anomalies))
+            res.end()
+        })
+    })
+}
+
+appPostAnomalies();
 
 console.log("Hello World");
 
@@ -63,25 +78,29 @@ function postInfo(res, result) {
     res.end()
 }
 
+function AppPostTableResults() {
+    app.post('/detect', (req, res) => {
+        if (req.files) {
+
+            const AnomaliesDetectorInput = new FormData()
+
+            AnomaliesDetectorInput.append("trainSetInput", req.files.trainSetInput.data)
+            AnomaliesDetectorInput.append("testSetInput", req.files.testSetInput.data)
+            AnomaliesDetectorInput.append("chosenAlgorithm", req.body.chosenAlgorithm)
+            console.log(req.body.chosenAlgorithm);
+            fetch(('http://localhost:8080/'), {
+                method: 'POST',
+                body: AnomaliesDetectorInput
+            }).then(result => result.json())
+                .then((result) => {
+
+                    postInfo(res, result);
+                })
+        }
+    })
+}
+
 //Post Method for '/search' url
-app.post('/detect', (req, res) => {
-    if (req.files) {
-
-        const AnomaliesDetectorInput = new FormData()
-
-        AnomaliesDetectorInput.append("trainSetInput", req.files.trainSetInput.data)
-        AnomaliesDetectorInput.append("testSetInput", req.files.testSetInput.data)
-        AnomaliesDetectorInput.append("chosenAlgorithm", req.body.chosenAlgorithm)
-        // console.log(req.body.chosenAlgorithm);
-        fetch(('http://localhost:8080/'), {
-            method: 'POST',
-            body: AnomaliesDetectorInput
-        }).then(result => result.json())
-            .then((result) => {
-
-                postInfo(res, result);
-            })
-    }
-})
+AppPostTableResults();
 
 app.listen(serverPort, () => console.log("Go to http://localhost:8080"))
