8db822708647044f129cd29f676bf92effab5480
OR MEMIA <or.memia@live.biu.ac.il>
2021-05-29 17:19:58 +0300

    detect updating

--- model/Algorithms/LinearAlgorithm.js
+++ model/Algorithms/LinearAlgorithm.js
@@ -15,31 +15,31 @@ class LinearAlgorithm {
         this.f = false;
     }
 
-    learnNormal(ts){
+    learnNormal(ts) {
         let atts = ts.getFeature()
 
         //init vals
         let vals = new Array(atts.length)
-        let numOfRows= ts.getNumOfInfoLines()
-        for (let i = 0; i < atts.length; i++){
+        let numOfRows = ts.getNumOfInfoLines()
+        for (let i = 0; i < atts.length; i++) {
             vals[i] = new Array(numOfRows);
         }
 
         //fill vals
-        for (let i = 0; i < atts.length; i++){
+        for (let i = 0; i < atts.length; i++) {
             let x = ts.getRowValuesOfFeature(atts[i])
-            for (let j = 0; j < numOfRows; j++){
+            for (let j = 0; j < numOfRows; j++) {
                 vals[i][j] = parseFloat(x[j]);
             }
         }
 
-        for (let i = 0; i < atts.length; i++){
+        for (let i = 0; i < atts.length; i++) {
             let f1 = atts[i];
             let max = 0;
             let jMax = 0;
 
             //find the most correlative
-            for (let j = i+1; j < atts.length; j++) {
+            for (let j = i + 1; j < atts.length; j++) {
                 let p = Math.abs(parseFloat(this.#anomalyDetectionUtil.pearson(vals[i], vals[j])))
                 if (p > max) {
                     max = p;
@@ -48,72 +48,72 @@ class LinearAlgorithm {
             }
 
             let f2 = atts[jMax];
-            let ps = this.toPoints(ts.getRowValuesOfFeature(f1),ts.getRowValuesOfFeature(f2));
-            this.learnHelper(ts,max,f1,f2,ps);
+            let ps = this.toPoints(ts.getRowValuesOfFeature(f1), ts.getRowValuesOfFeature(f2));
+            this.learnHelper(ts, max, f1, f2, ps);
         }
     }
 
-    toPoints(v1,v2){
+    toPoints(v1, v2) {
         let ps = new Array(v1.length)
-        for (let i = 0; i < v1.length; i++){
+        for (let i = 0; i < v1.length; i++) {
             ps[i] = new Shapes.Point(parseFloat(v1[i]), parseFloat(v2[i]));
         }
         return ps;
     }
 
-    detect(ts){
-        let v = [];
-        for (let i = 0; i < this.#cf.length; i++){
-            let currentCorrelatedFeatures = this.#cf[i];
-            let x = ts.getRowValuesOfFeature(currentCorrelatedFeatures.feature1);
-            let y = ts.getRowValuesOfFeature(currentCorrelatedFeatures.feature2);
-            for(let j= 0; j < x.length; j++){
-                if(this.isDetect(x[j],y[j],currentCorrelatedFeatures)){
-                    let d = currentCorrelatedFeatures.feature1 + " + " + currentCorrelatedFeatures.feature2;
-                    v.push(new AnomalyReport(d,(j+1)));
+    detect(ts) {
+        let anomaly = [];
+        for (let i = 0; i < this.#cf.length; i++) {
+            let correlatedFeatures = this.#cf[i];
+            let x = ts.getRowValuesOfFeature(correlatedFeatures.feature1);
+            let y = ts.getRowValuesOfFeature(correlatedFeatures.feature2);
+            for (let j = 0; j < x.length; j++) {
+                if (this.isDetect(x[j], y[j], correlatedFeatures)) {
+                    let det = correlatedFeatures.feature1 + "-" + correlatedFeatures.feature2;
+                    anomaly.push(new AnomalyReport(det,(j+1)));
                 }
             }
         }
-        return v;
+        return anomaly;
     }
 
-    getCf(){
+    getCf() {
         return this.#cf
     }
 
-    setCorrelationThreshold(newThreshold){
+    setCorrelationThreshold(newThreshold) {
         this.#threshold = newThreshold;
     }
 
-    learnHelper(ts, pearson, f1, f2, points){
-        if(pearson>this.#threshold){
+    learnHelper(ts, pearson, f1, f2, points) {
+        if (pearson > this.#threshold) {
             let len = ts.getNumOfInfoLines();
             let c = new CorrelatedFeatures();
-            c.feature1=f1;
-            c.feature2=f2;
-            c.corrlation=parseFloat(pearson);
-            c.lin_reg=this.#anomalyDetectionUtil.linear_reg(points);
-            c.threshold=this.findThreshold(points,len,c.lin_reg)*1.1; // 10% increase
+            c.feature1 = f1;
+            c.feature2 = f2;
+            c.corrlation = parseFloat(pearson);
+            c.lin_reg = this.#anomalyDetectionUtil.linear_reg(points);
+            c.threshold = this.findThreshold(points, len, c.lin_reg) * 1.1; // 10% increase
             this.#cf.push(c);
         }
     }
 
-    isDetect(x, y, correlatedFeatures){
+    isDetect(x, y, correlatedFeatures) {
         return (Math.abs(y - correlatedFeatures.lin_reg.f(x)) > correlatedFeatures.threshold);
     }
 
-    findThreshold(points, len, rl){
+    findThreshold(points, len, rl) {
         let max = 0;
-        for (let i = 0; i < len; i++){
+        for (let i = 0; i < len; i++) {
             let d = Math.abs(parseFloat(points[i].y) - parseFloat(rl.f(parseFloat(points[i].x))))
-            if(d > max){
+            if (d > max) {
                 max = d;
             }
         }
         return max;
     }
 
-    getThreshold(){
+    getThreshold() {
         return this.#threshold;
     }
 }
